3261435d9d896fbfedece5b831692c336e225a32
HemantSharma909 <104320688+HemantSharma909@users.noreply.github.com>
2022-04-25 00:04:14 +0530

    Add files via upload

---
+++ python assignment 1 output.py
@@ -0,0 +1,35 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/heman/AppData/Local/Programs/Python/Python310/assign 1 ques 1 sol.py
+Give three numbers to calculate average
+a=10
+b=10
+c=20
+average of given numbers is 13.333333333333334
+
+======== RESTART: C:/Users/heman/OneDrive/Desktop/assign 1 ques 2 sol.py =======
+Enter your gross income($)=50000
+Enter number of dependents=3
+Your Taxable income is 31000.0
+Your Tax is 6200.0
+
+======== RESTART: C:/Users/heman/OneDrive/Desktop/assign 1 ques 3 sol.py =======
+Enter SID: 21102103
+Enter Name: Hemant Sharma
+Enter Gender(M(male),F(female),U for unknown)M
+Enter course name: Pyhton
+Enter CGPA10
+['21102103', 'Hemant Sharma', 'M', 'Pyhton', 10.0]
+
+======== RESTART: C:/Users/heman/OneDrive/Desktop/assign 1 ques 3 sol.py =======
+Enter Marks 1 : 40
+Enter Marks 2 : 42
+Enter Marks 3 : 44
+Enter Marks 4 : 46
+Enter Marks 5 : 32
+Sorted Order Of Marks :  [32, 40, 42, 44, 46]
+
+======== RESTART: C:/Users/heman/OneDrive/Desktop/assign 1 ques 3 sol.py =======
+color: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+color : ['Red', 'Green', 'White', 'Purple', 'Yellow']
