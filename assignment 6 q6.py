73505fa3ed5b6ea030907b481927120b6920519f
HemantSharma909 <104320688+HemantSharma909@users.noreply.github.com>
2022-07-03 22:51:32 +0530

    Delete assignment 6 q6.py

--- assignment 6 q6.py
+++
@@ -1,4 +0,0 @@
-#Q.6
-def student(student_id, student_name, student_class):
-    return f'Student ID: {student_id}\nStudent Name: {student_name}\nClass: {student_class}'
-print(student('21102103', 'Hemant Sharma', 'B.Tech_1st_year'))
