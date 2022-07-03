50663bdb7d7c2477062ea24eda7d6ff6070f194c
HemantSharma909 <104320688+HemantSharma909@users.noreply.github.com>
2022-07-03 22:51:20 +0530

    Delete assignment 6 q5.py

--- assignment 6 q5.py
+++
@@ -1,5 +0,0 @@
-#Q.5
-items=[n for n in input("enter the words: ").split()]       
-items.sort()                                             
-print("alphabetically arranged words:")
-print('-'.join(items))
